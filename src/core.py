1e693553e620bda11b3926fe4868cefd07cdc7ca
Kristen Foster-Marks <kristen.foster-marks@zayo.com>
2019-09-28 09:09:38 -0600

    Write basic get_token method and add three tests

--- src/core.py
+++ src/core.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+import spacy
 
-def hello_world():
-    return 'Hello, world.'
+nlp = spacy.load("en_core_web_sm")
+
+def get_tokens(text):
+    tokens = []
+    doc = nlp.make_doc(text)
+
+    for token in doc:
+        tokens.append(token.text)
+    print(tokens)
+    return tokens
--- tests/test.py
+++ tests/test.py
@@ -7,8 +7,15 @@ import unittest
 class AdvancedTestSuite(unittest.TestCase):
     """Advanced test cases."""
 
-    def test_hello_world(self):
-        assert src.hello_world() == 'Hello, world.'
+    def test_get_tokens(self):
+        text = "Denver is the biggest city in Colorado."
+        assert src.get_tokens(text) == ['Denver', 'is', 'the', 'biggest', 'city', 'in', 'Colorado', '.']
+
+        text2 = "Life is good :)"
+        assert src.get_tokens(text2) == ['Life', 'is', 'good', ':)']
+
+        text3 = "We spent nearly 4 million dollars in N.Y.C.!"
+        assert src.get_tokens(text3) == ['We', 'spent', 'nearly', '4', 'million', 'dollars', 'in', 'N.Y.C.', '!']
 
 if __name__ == '__main__':
     unittest.main()
