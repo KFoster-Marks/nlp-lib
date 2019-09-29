0055b8b7c44daa5434b912bef6789c159230fa79
Kristen Foster-Marks <kristen.foster-marks@zayo.com>
2019-09-29 05:14:56 -0600

    Add get_noun_phrases and tests

--- src/core.py
+++ src/core.py
@@ -9,5 +9,21 @@ def get_tokens(text):
 
     for token in doc:
         tokens.append(token.text)
-    print(tokens)
+
     return tokens
+
+def get_named_entities(text):
+    entities = []
+    doc = nlp(text)
+    for ent in doc.ents:
+        entities.append(ent.text)
+
+    return entities
+
+def get_base_noun_phrases(text):
+    noun_phrases = []
+    doc = nlp(text)
+    for phrase in doc.noun_chunks:
+        noun_phrases.append(phrase.text)
+    print(noun_phrases)
+    return noun_phrases
--- tests/test.py
+++ tests/test.py
@@ -8,14 +8,25 @@ class AdvancedTestSuite(unittest.TestCase):
     """Advanced test cases."""
 
     def test_get_tokens(self):
-        text = "Denver is the biggest city in Colorado."
+        text = 'Denver is the biggest city in Colorado.'
         assert src.get_tokens(text) == ['Denver', 'is', 'the', 'biggest', 'city', 'in', 'Colorado', '.']
 
-        text2 = "Life is good :)"
+        text2 = 'Life is good :)'
         assert src.get_tokens(text2) == ['Life', 'is', 'good', ':)']
 
-        text3 = "We spent nearly 4 million dollars in N.Y.C.!"
+        text3 = 'We spent nearly 4 million dollars in N.Y.C.!'
         assert src.get_tokens(text3) == ['We', 'spent', 'nearly', '4', 'million', 'dollars', 'in', 'N.Y.C.', '!']
+
+    def test_get_named_entities(self):
+        text = 'Ms. Harris ordered the National Guard to the Canadian border.'
+        assert src.get_named_entities(text) == ['Harris', 'the National Guard', 'Canadian']
+
+    def test_get_noun_phrases(self):
+        text = 'The doughnuts fell into my belly with disconcerting speed.'
+        assert src.get_base_noun_phrases(text) == ['The doughnuts', 'my belly', 'disconcerting speed']
+
+        text2 = 'A national park system must be maintained by a concerned and capable group of outdoor enthusiasts and animal lovers.'
+        assert src.get_base_noun_phrases(text2) == ['A national park system', 'a concerned and capable group', 'outdoor enthusiasts', 'animal lovers']
 
 if __name__ == '__main__':
     unittest.main()
