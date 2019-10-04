4bbad571fff07ad892beceff90e9ae81e882ea04
Kristen Foster-Marks <kristen.foster-marks@zayo.com>
2019-10-04 05:37:54 -0600

    Fix lemmatize return & test

--- src/lemmatize.py
+++ src/lemmatize.py
@@ -5,9 +5,11 @@ nlp = spacy.load("en_core_web_sm")
 def get_lemmas(text):
     doc = nlp(text)
     parse_string = ''
+    lemmas = []
 
     for token in doc:
+        lemmas.append(str(token.lemma_))
         parse_string = f'{token.text:{12}} | {token.pos_:{5}} | {token.lemma_}'
         print(parse_string)
 
-    return parse_string
+    return lemmas
--- tests/test_lemmatize.py
+++ tests/test_lemmatize.py
@@ -7,16 +7,7 @@ class LemmatizeTestSuit(unittest.TestCase):
 
     def test_get_lemmas(self):
         text = 'This accuser alleges a whole different category of impropriety.'
-        assert src.get_lemmas(text) == 'This         | DET   | this
-accuser      | NOUN  | accuser
-alleges      | VERB  | allege
-a            | DET   | a
-whole        | ADJ   | whole
-different    | ADJ   | different
-category     | NOUN  | category
-of           | ADP   | of
-impropriety  | NOUN  | impropriety
-.            | PUNCT | .'
+        assert src.get_lemmas(text) == ['this', 'accuser', 'allege', 'a', 'whole', 'different', 'category', 'of', 'impropriety', '.']
 
 if __name__ == '__main__':
     unittest.main()
