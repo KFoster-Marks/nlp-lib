085b57bc1b5eeb4ffbe28e0614c0d31b7d0d30aa
Kristen Foster-Marks <kristen.foster-marks@zayo.com>
2019-10-30 06:37:13 -0600

    Add test for altenate punct like qm, excl., ellipses

--- tests/test_segment.py
+++ tests/test_segment.py
@@ -12,6 +12,9 @@ class SegmentTestSuite(unittest.TestCase):
 
         assert src.get_segments(text) == ['My husband is going for a long run.', 'He will run for about four hours.', 'He will run around the ponds.']
 
+        text2 = u'The weekend has arrived! What will we do? We will play...'
+        assert src.get_segments(text2) == ['The weekend has arrived!', 'What will we do?', 'We will play...']
+
     def test_get_custom_segment(self):
         text = u'Not everyone agrees on the best way to build software; many argue over the costs and benefits of waterfall versus agile, for example.'
 
